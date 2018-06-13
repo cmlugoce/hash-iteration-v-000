9e793163a39e38432af2855d8dd263ed942ee755
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 13:51:51 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,6 +1,15 @@
 def select_winner(passengers)
-  
+  passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
+
+
+end
 # passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
