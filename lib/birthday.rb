6c780bc88486c64c4a17f53e1bbd085491b33b84
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 13:55:14 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,10 @@ def happy_birthday(birthday_kids)
     "Amanda" => 27
 }
 
-
+birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
 end
 
